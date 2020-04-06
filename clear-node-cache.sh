4c746a0b60b774bb8e193301f9667e8ce29c076e
Arnaud Vergnet <vergnet@etud.insa-toulouse.fr>
2020-04-06 14:34:44 +0200

    Improved script

--- clear-node-cache.sh
+++ clear-node-cache.sh
@@ -1,1 +1,20 @@
-rm -rf node_modules/ && rm -f package-lock.json && rm -f yarn.lock && npm cache verify && npm install && expo r -c
+#!/bin/bash
+
+echo "Removing node_modules..."
+rm -rf node_modules/
+echo -e "Done\n"
+
+echo "Removing locks..."
+rm -f package-lock.json && rm -f yarn.lock
+echo -e "Done\n"
+
+#echo "Verifying npm cache..."
+#npm cache verify
+#echo -e "Done\n"
+
+echo "Installing dependencies..."
+npm install
+echo -e "Done\n"
+
+echo "Starting expo with clear cache..."
+expo r -c
