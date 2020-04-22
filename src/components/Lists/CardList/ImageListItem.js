62db99cac7df5c3c466004a4bb4c1b4f90011dff
Arnaud Vergnet <vergnet@etud.insa-toulouse.fr>
2020-04-22 14:58:08 +0200

    Reduced image size

--- src/components/Lists/CardList/ImageListItem.js
+++ src/components/Lists/CardList/ImageListItem.js
@@ -33,8 +33,8 @@ export default class ImageListItem extends React.Component<Props> {
                 <View>
                     <Image
                         style={{
-                            width: 100,
-                            height: 100,
+                            width: 80,
+                            height: 80,
                             marginLeft: 'auto',
                             marginRight: 'auto',
                         }}
