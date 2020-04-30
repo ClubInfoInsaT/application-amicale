1835fcadf967e28864a4954fc93f3e5eb2d0430c
Arnaud Vergnet <vergnet@etud.insa-toulouse.fr>
2020-04-30 22:16:35 +0200

    Added tests for proxiwash util functions

--- src/utils/Proxiwash.js
+++ src/utils/Proxiwash.js
@@ -1,13 +1,19 @@
 // @flow
 
 import type {Machine} from "../screens/Proxiwash/ProxiwashScreen";
-import ProxiwashConstants from "../constants/ProxiwashConstants";
 
+/**
+ * Gets the machine end Date object.
+ * If the time is before the current time, it will be considered as tomorrow
+ *
+ * @param machine The machine to get the date from
+ * @returns {Date} The date object representing the end time.
+ */
 export function getMachineEndDate(machine: Machine) {
     const array = machine.endTime.split(":");
-    let date = new Date();
+    let date = new Date(Date.now());
     date.setHours(parseInt(array[0]), parseInt(array[1]));
-    if (date < new Date())
+    if (date < new Date(Date.now()))
         date.setDate(date.getDate() + 1);
     return date;
 }
@@ -15,8 +21,8 @@ export function getMachineEndDate(machine: Machine) {
 /**
  * Checks whether the machine of the given ID has scheduled notifications
  *
- * @param machine
- * @param machineList
+ * @param machine The machine to check
+ * @param machineList The machine list
  * @returns {boolean}
  */
 export function isMachineWatched(machine: Machine, machineList: Array<Machine>) {
@@ -30,7 +36,14 @@ export function isMachineWatched(machine: Machine, machineList: Array<Machine>) 
     return watched;
 }
 
-function getMachineOfId(id: string, allMachines: Array<Machine>) {
+/**
+ * Gets the machine of the given id
+ *
+ * @param id The machine's ID
+ * @param allMachines The machine list
+ * @returns {null|Machine} The machine or null if not found
+ */
+export function getMachineOfId(id: string, allMachines: Array<Machine>) {
     for (let i = 0; i < allMachines.length; i++) {
         if (allMachines[i].number === id)
             return allMachines[i];
@@ -38,13 +51,22 @@ function getMachineOfId(id: string, allMachines: Array<Machine>) {
     return null;
 }
 
-export function getCleanedMachineWatched(machineList: Array<Machine>, allMachines: Array<Machine>) {
+/**
+ * Gets a cleaned machine watched list by removing invalid entries.
+ * An entry is considered invalid if the end time in the watched list
+ * and in the full list does not match (a new machine cycle started)
+ *
+ * @param machineWatchedList The current machine watch list
+ * @param allMachines The current full machine list
+ * @returns {Array<Machine>}
+ */
+export function getCleanedMachineWatched(machineWatchedList: Array<Machine>, allMachines: Array<Machine>) {
     let newList = [];
-    for (let i = 0; i < machineList.length; i++) {
-        let machine = getMachineOfId(machineList[i].number, allMachines);
+    for (let i = 0; i < machineWatchedList.length; i++) {
+        let machine = getMachineOfId(machineWatchedList[i].number, allMachines);
         if (machine !== null
-            && machineList[i].number === machine.number && machineList[i].endTime === machine.endTime
-            && ProxiwashConstants.machineStates[machineList[i].state] === ProxiwashConstants.machineStates["EN COURS"]) {
+            && machineWatchedList[i].number === machine.number
+            && machineWatchedList[i].endTime === machine.endTime) {
             newList.push(machine);
         }
     }
