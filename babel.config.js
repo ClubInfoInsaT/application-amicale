54486d1deb0a6b1fe2806ca79597989ef9418b0a
Arnaud Vergnet <arnaud.vergnet@mailo.com>
2020-09-21 21:14:42 +0200

    Update project config files to use TypeScript

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset', '@babel/preset-flow'],
+  presets: ['module:metro-react-native-babel-preset'],
+  env: {
+    production: {
+      plugins: ['react-native-paper/babel'],
+    },
+  },
 };
