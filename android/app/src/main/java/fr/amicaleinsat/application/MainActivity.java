89c65d70705abf6c1517c303f0fbef373fc6e1b4
Arnaud Vergnet <arnaud.vergnet@mailo.com>
2020-09-21 19:01:32 +0200

    Improve android splash screen style

--- android/app/src/main/java/fr/amicaleinsat/application/MainActivity.java
+++ android/app/src/main/java/fr/amicaleinsat/application/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends ReactActivity {
 
    @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);
+        SplashScreen.show(this, R.style.SplashScreenTheme);
         super.onCreate(savedInstanceState);
     }
 
