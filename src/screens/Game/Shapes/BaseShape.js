cc03aa69121ed27bfed9104da4ea4c3fc1718072
Arnaud Vergnet <arnaud.vergnet@mailo.com>
2020-09-21 18:12:58 +0200

    Add licence headers

--- src/screens/Game/Shapes/BaseShape.js
+++ src/screens/Game/Shapes/BaseShape.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2019 - 2020 Arnaud Vergnet.
+ *
+ * This file is part of Campus INSAT.
+ *
+ * Campus INSAT is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Campus INSAT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Campus INSAT.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // @flow
 
 import type {CustomThemeType} from '../../../managers/ThemeManager';
