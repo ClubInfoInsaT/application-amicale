be298728851a1f2fc93daf1c4e5627e5f3a594aa
Paul ALNET <paul@shimaore.net>
2022-11-02 11:57:54 +0100

    notif : remove unused imports

--- src/utils/Notifications.ts
+++ src/utils/Notifications.ts
@@ -24,9 +24,7 @@ import PushNotification, {
   PushNotificationObject,
 } from 'react-native-push-notification';
 import { Platform } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 import Update from '../constants/Update';
-import { MainRoutes } from '../navigation/MainNavigator';
 
 // Used to multiply the normal notification id to create the reminder one. It allows to find it back easily
 const reminderIdFactor = 100;
