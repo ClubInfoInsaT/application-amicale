902bda3824ae702407c0e4bc41ec0f5392539e23
Paul ALNET <alnet@insa-toulouse.fr>
2024-12-17 11:14:11 +0100

    fix(proxiwash): website url

--- src/constants/Urls.tsx
+++ src/constants/Urls.tsx
@@ -29,8 +29,7 @@ const PROXIMO_ENDPOINT = STUDENT_SERVER + '~proximo/v2/api/';
 const PROXIMO_IMAGES_ENDPOINT =
   STUDENT_SERVER + '~proximo/api_proximo/storage/app/public/';
 const APP_IMAGES_ENDPOINT = STUDENT_SERVER + '~amicale_app/images/';
-const PROXIWASH_ENDPOINT =
-  'https://www.proxiwash.com/weblaverie/ma-laverie-2?s=';
+const PROXIWASH_ENDPOINT = 'https://www.proxiwash.com/laverie?code=';
 
 export default {
   amicale: {
