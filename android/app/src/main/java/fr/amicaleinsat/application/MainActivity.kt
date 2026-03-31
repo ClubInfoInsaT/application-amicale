f30c285b743fe0cdf171f4d337e322c93c1a4ca4
Paul ALNET <alnet@insa-toulouse.fr>
2026-03-31 11:40:59 +0200

    fix: android underterministic crash
    Introduced in recent RN upgrade. Same as #20.
    
    This time I found the
    [issue](https://github.com/software-mansion/react-native-screens/issues/17)
    in the library thanks to a crash log.

--- android/app/src/main/java/fr/amicaleinsat/application/MainActivity.kt
+++ android/app/src/main/java/fr/amicaleinsat/application/MainActivity.kt
@@ -3,6 +3,8 @@ package fr.amicaleinsat.application
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import android.os.Bundle;
+import com.swmansion.rnscreens.fragment.restoration.RNScreensFragmentFactory;
 
 class MainActivity : ReactActivity() {
 
@@ -11,5 +13,11 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "Campus"
+
+  // From https://github.com/software-mansion/react-native-screens?tab=readme-ov-file#android
+  override fun onCreate(savedInstanceState: Bundle?) {
+    supportFragmentManager.fragmentFactory = RNScreensFragmentFactory()
+    super.onCreate(savedInstanceState);
+  }
 }
 
