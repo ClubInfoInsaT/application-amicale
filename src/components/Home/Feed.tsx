05383892c3a1acce8184cd5bbd6ec22d10bbcbc6
Paul ALNET <alnet@insa-toulouse.fr>
2026-08-19 18:39:41 +0200

    fix(feed): scroll entire screen instead of just posts

--- src/components/Home/Feed.tsx
+++ src/components/Home/Feed.tsx
@@ -146,6 +146,7 @@ function Feed() {
 
   return (
     <WebSectionList
+      scrollEnabled={false}
       request={request}
       createDataset={createDataset}
       autoRefreshTime={REFRESH_TIME}
